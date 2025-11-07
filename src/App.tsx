53e69eab49e88840a045632c8768118266efcbb5
Álvaro Braz <alvarobraz83@gmail.com>
2025-11-07 18:32:09 -0300

    feat: created text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-pink-dark">Olá mundo!</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="heading-lg" className="text-green-200">
+        Olá mundo!
+      </Text>
+      <Text className="heading-lg">Olá mundo!</Text>
+      <Text variant="body-md-regular">Olá mundo!</Text>
+      <Text variant="text-label">Olá mundo!</Text>
+      <Text variant="title-bold">Olá mundo!</Text>
+      <Text variant="sub-title">Olá mundo!</Text>
+      <Text>Levar o dog pra passear</Text>
+    </div>
+  );
 }
