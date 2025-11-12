e67a123377baa03d65c53b3e951e7ec271452379
Álvaro Braz <alvarobraz83@gmail.com>
2025-11-12 01:45:05 -0300

    feat: prepared api helper and configure react query and first fetcher

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import PageComponents from "./pages/page-components";
 import LayoutMain from "./pages/layout-main";
 import PageHome from "./pages/page-home";
 import PageRefund from "./pages/page-refund";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<LayoutMain />}>
-          <Route index element={<PageHome />} />
-          <Route path="/refund-request/:id" element={<PageRefund />} />
-          <Route path="/refund-request/" element={<PageRefund />} />
-          <Route path="/componentes" element={<PageComponents />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutMain />}>
+            <Route index element={<PageHome />} />
+            <Route path="/refund-request/:id" element={<PageRefund />} />
+            <Route path="/refund-request/" element={<PageRefund />} />
+            <Route path="/componentes" element={<PageComponents />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
