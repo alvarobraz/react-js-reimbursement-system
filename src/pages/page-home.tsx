04fb44ad719cb00091b9546213d0c3e6bcd929b2
Álvaro Braz <alvarobraz83@gmail.com>
2025-11-13 02:18:59 -0300

    feat: add pagination

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -7,9 +7,14 @@ import ButtonIcon from "../components/button-icon";
 import CaretLeft from "../assets/icons/caret-left.svg?react";
 import CaretRight from "../assets/icons/caret-right.svg?react";
 import useRefunds from "../contexts/refund/hooks/use-refunds";
+import { useNavigate } from "react-router";
 
 export default function PageHome() {
-  const { isLoadingRefunds, refunds } = useRefunds();
+  const { isLoadingRefunds, refunds, meta } = useRefunds();
+  const navigate = useNavigate();
+
+  console.log("meta => " + JSON.stringify(meta));
+  // nextPageUrl
 
   return (
     <Container className="flex flex-col gap-6">
@@ -18,11 +23,27 @@ export default function PageHome() {
       <Divider />
       <RefundList refunds={refunds} loading={isLoadingRefunds} />
       <div className="flex justify-center items-center">
-        <ButtonIcon size="sm" icon={CaretLeft} variant="primary" />
+        <ButtonIcon
+          size="sm"
+          icon={CaretLeft}
+          variant="primary"
+          onClick={() => {
+            navigate(`${meta?.previousPageUrl}`);
+          }}
+          disabled={meta?.previousPageUrl === null}
+        />
         <Text variant="body-md-regular" className="px-2">
-          1/3
+          {meta?.currentPage + "/" + meta?.lastPage}
         </Text>
-        <ButtonIcon size="sm" icon={CaretRight} variant="primary" />
+        <ButtonIcon
+          size="sm"
+          icon={CaretRight}
+          variant="primary"
+          onClick={() => {
+            navigate(`${meta?.nextPageUrl}`);
+          }}
+          disabled={meta?.nextPageUrl === null}
+        />
       </div>
     </Container>
   );
